3206f2bdfda8529bf156eb5f0abd4cd7c1da76fc
Naser Ansari-Pour <42205757+nansari-pour@users.noreply.github.com>
2023-04-20 02:44:33 +0100

    Update gnomADextractor.sh

--- gnomADextractor.sh
+++ gnomADextractor.sh
@@ -1,16 +1,15 @@
-# SLURM-based pipeline for extracting gnomAD allele frequency (AF) for WGS/WES genomic variants
-
 #!/bin/bash
-
-WORKDIR=$1
-vcf_file=$2
-
 #SBATCH --partition=batch
 #SBATCH --job-name=AF
 #SBATCH --mem=10GB
-#SBATCH --time=2:00:00
-#SBATCH --output=${WORKDIR}AF_%j.outerr
-#SBATCH --error=${WORKDIR}AF_%j.outerr
+#SBATCH --time=1:00:00
+#SBATCH --output=AF_%j.outerr
+#SBATCH --error=AF_%j.outerr
+
+# SLURM-based pipeline for extracting gnomAD allele frequency (AF) for WGS/WES genomic variants
+
+WORKDIR=$1
+vcf_file=$2
 
 # load the R module on the SLURM cluster - edit appropriately
 module load R-cbrg/current
